24fe7e1bc83fe39bf72624303099c402ac40a3dd
Orfeas <orfeas126@gmail.com>
2020-06-28 00:08:37 -0700

    Fixed bugs and added permissions

--- src/main/java/com/orf9/antixrayplus/commandAntiXrayPlus.java
+++ src/main/java/com/orf9/antixrayplus/commandAntiXrayPlus.java
@@ -17,38 +17,38 @@ public class commandAntiXrayPlus implements CommandExecutor {
         if (sender instanceof Player) {
 
             Player player = (Player) sender;
+            if (player.hasPermission("antixrayplus.use")) {
+                if (args.length == 1) {
+                    Player target = Bukkit.getPlayer(args[0]);
+                    if (target != null) {
+                        String uuid = target.getUniqueId().toString();
 
-            if (args.length == 1) {
-                Player target = Bukkit.getPlayer(args[0]);
-                if(target != null) {
-                    String uuid = target.getUniqueId().toString();
+                        int stoneMined = (int) main.getPlayerData().get(uuid + ".stoneMined");
+                        int diamondOreMined = (int) main.getPlayerData().get(uuid + ".diamondOreMined");
+                        double ratio = (double) main.getPlayerData().get(uuid + ".ratio");
 
-                    int stoneMined = (int) main.getPlayerData().get(uuid + ".stoneMined");
-                    int diamondOreMined = (int) main.getPlayerData().get(uuid + ".diamondOreMined");
-                    double ratio = (double) main.getPlayerData().get(uuid + ".ratio");
+                        player.sendMessage("Blocks mined by: " + ChatColor.GREEN + target.getName());
+                        player.sendMessage("Diamond Ore: " + diamondOreMined);
+                        player.sendMessage("Stone: " + stoneMined);
+                        player.sendMessage("Ratio of Diamond to Stone: " + ratio);
 
-                    player.sendMessage("Blocks mined by: " + ChatColor.GREEN + target.getName());
-                    player.sendMessage("Diamond Ore: " + diamondOreMined);
-                    player.sendMessage("Stone: " + stoneMined);
-                    player.sendMessage("Ratio of Diamond to Stone: " + ratio);
+                        double maxRatio = main.getConfig().getDouble("threshold.max-ratio");
+                        int minStone = main.getConfig().getInt("threshold.stone-minimum");
+                        int minDiamondOre = main.getConfig().getInt("threshold.diamond-ore-minimum");
 
-                    double maxRatio = main.getConfig().getDouble("threshold.max-ratio");
-                    int minStone = main.getConfig().getInt("threshold.stone-minimum");
-                    int minDiamondOre = main.getConfig().getInt("threshold.diamond-ore-minimum");
+                        if (ratio > maxRatio && stoneMined > minStone && diamondOreMined > minDiamondOre) {
+                            player.sendMessage(ChatColor.RED + "Player may be using cheats! Ratio: " + ratio);
+                        }
 
-                    if (ratio > maxRatio && stoneMined > minStone && diamondOreMined > minDiamondOre){
-                        player.sendMessage(ChatColor.RED + "Player may be using cheats! Ratio: " + ratio);
+                    } else {
+                        player.sendMessage("Invalid Arguments! Usage: /antixrayplus [player]");
                     }
 
-                }else {
+                } else {
                     player.sendMessage("Invalid Arguments! Usage: /antixrayplus [player]");
                 }
-
             } else {
-                player.sendMessage("Invalid Arguments! Usage: /antixrayplus [player]");
-
-
-
+                player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
             }
         } else {
             sender.sendMessage("This command can only be used in-game!");
