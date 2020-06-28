65506094772478394deb708448659c305d98718f
Orfeas <orfeas126@gmail.com>
2020-06-28 15:55:35 -0700

    1.0.7
    -Major Optimizations
    -Added support for commands through console!

--- src/main/java/com/orf9/antixrayplus/Main.java
+++ src/main/java/com/orf9/antixrayplus/Main.java
@@ -23,8 +23,13 @@ public final class Main extends JavaPlugin implements Listener {
     private File playerDataFile;
     private YamlConfiguration modifyPlayerData;
 
-    public YamlConfiguration getPlayerData(){ return modifyPlayerData; }
-    public File getPlayerDataFile(){ return playerDataFile; }
+    public YamlConfiguration getPlayerData() {
+        return modifyPlayerData;
+    }
+
+    public File getPlayerDataFile() {
+        return playerDataFile;
+    }
 
     @Override
     public void onEnable() {
@@ -53,7 +58,7 @@ public final class Main extends JavaPlugin implements Listener {
     public void initiateFiles() throws IOException {
         playerDataFile = new File(this.getDataFolder(), "playerdata.yml");
 
-        if (!playerDataFile.exists()){
+        if (!playerDataFile.exists()) {
             playerDataFile.createNewFile();
         }
         modifyPlayerData = YamlConfiguration.loadConfiguration(playerDataFile);
@@ -61,100 +66,101 @@ public final class Main extends JavaPlugin implements Listener {
     }
 
 
-
     @EventHandler
-    public void onMine(BlockBreakEvent e){
+    public void onMine(BlockBreakEvent e) {
         Player player = e.getPlayer();
         String uuid = player.getUniqueId().toString();
         Block block = e.getBlock();
+        if (block.getType().equals(Material.DIAMOND_ORE) || block.getType().equals(Material.STONE)) {
 
-        int oldStoneMined = (int) getPlayerData().get(uuid + ".stoneMined");
-        int oldDiamondOreMined = (int) getPlayerData().get(uuid + ".diamondOreMined");
+            int oldStoneMined = (int) getPlayerData().get(uuid + ".stoneMined");
+            int oldDiamondOreMined = (int) getPlayerData().get(uuid + ".diamondOreMined");
 
-        int stoneMined = oldStoneMined;
-        int diamondOreMined = oldDiamondOreMined;
+            int stoneMined = oldStoneMined;
+            int diamondOreMined = oldDiamondOreMined;
 
-        double ratio;
-        boolean wasDiamond = false;
+            double ratio;
+            boolean wasDiamond = false;
 
-        if (block.getType().equals(Material.DIAMOND_ORE)){
-            ItemStack item = player.getInventory().getItemInMainHand();
-            if (!item.containsEnchantment(Enchantment.SILK_TOUCH)){
-                diamondOreMined++;
-                getPlayerData().set(uuid + ".diamondOreMined", diamondOreMined);
-                wasDiamond = true;
+            if (block.getType().equals(Material.DIAMOND_ORE)) {
+                ItemStack item = player.getInventory().getItemInMainHand();
+                if (!item.containsEnchantment(Enchantment.SILK_TOUCH)) {
+                    diamondOreMined++;
+                    getPlayerData().set(uuid + ".diamondOreMined", diamondOreMined);
+                    wasDiamond = true;
+                    try {
+                        getPlayerData().save(getPlayerDataFile());
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                }
+            } else if (block.getType().equals(Material.STONE)) {
+                stoneMined++;
+                getPlayerData().set(uuid + ".stoneMined", stoneMined);
                 try {
                     getPlayerData().save(getPlayerDataFile());
                 } catch (IOException ioException) {
                     ioException.printStackTrace();
                 }
             }
-        }else if (block.getType().equals(Material.STONE)) {
-            stoneMined++;
-            getPlayerData().set(uuid + ".stoneMined", stoneMined);
-            try {
-                getPlayerData().save(getPlayerDataFile());
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
-        }
-        if (stoneMined != oldStoneMined || diamondOreMined != oldDiamondOreMined){
-            if (!(diamondOreMined == 0) && !(stoneMined == 0)){
-                ratio = (double) diamondOreMined / (double) stoneMined;
-                getPlayerData().set(uuid + ".ratio", ratio);
+            if (stoneMined != oldStoneMined || diamondOreMined != oldDiamondOreMined) {
+                if (!(diamondOreMined == 0) && !(stoneMined == 0)) {
+                    ratio = (double) diamondOreMined / (double) stoneMined;
+                    getPlayerData().set(uuid + ".ratio", ratio);
 
-                Double maxRatio = getConfig().getDouble("maxRatio");
+                    Double maxRatio = getConfig().getDouble("maxRatio");
 
-                int minStone = getConfig().getInt("stoneMinimum");
-                int minDiamondOre = getConfig().getInt("diamondMinimum");
-                if (minStone == 0){
-                    minStone = 1024;
-                }
-                if (minDiamondOre == 0){
-                    minDiamondOre = 32;
-                }
-                if (maxRatio == 0){
-                    maxRatio = 0.002;
-                }
+                    int minStone = getConfig().getInt("stoneMinimum");
+                    int minDiamondOre = getConfig().getInt("diamondMinimum");
+                    if (minStone == 0) {
+                        minStone = 1024;
+                    }
+                    if (minDiamondOre == 0) {
+                        minDiamondOre = 32;
+                    }
+                    if (maxRatio == 0) {
+                        maxRatio = 0.002;
+                    }
 
-                if (stoneMined > minStone ) {
+                    if (stoneMined > minStone) {
 
 
-                    if (diamondOreMined > minDiamondOre) {
+                        if (diamondOreMined > minDiamondOre) {
 
-                        if (ratio > maxRatio || wasDiamond == true) {
+                            if (ratio > maxRatio || wasDiamond) {
 
-                            getLogger().log(Level.WARNING, "Player " + player.getName() + " may be using cheats! Ratio: " + ratio);
+                                getLogger().log(Level.WARNING, "Player " + player.getName() + " may be using cheats! Ratio: " + ratio);
 
-                            Bukkit.getOnlinePlayers().forEach(pl -> {
-                                if (pl.hasPermission("antixrayplus.alerts")) {
-                                    pl.sendMessage(ChatColor.RED + "Player " + player + " may be using XRAY! Ratio:" + ratio);
-                                }
-                            });
+                                Bukkit.getOnlinePlayers().forEach(pl -> {
+                                    if (pl.hasPermission("antixrayplus.alerts")) {
+                                        pl.sendMessage(ChatColor.RED + "Player " + player + " may be using XRAY! Ratio:" + ratio);
+                                    }
+                                });
 
+                            }
                         }
                     }
                 }
-            }
 
-            getPlayerData().set(uuid + ".diamondOreMined", diamondOreMined);
-            getPlayerData().set(uuid + ".stoneMined", stoneMined);
+                getPlayerData().set(uuid + ".diamondOreMined", diamondOreMined);
+                getPlayerData().set(uuid + ".stoneMined", stoneMined);
 
 
-            try {
-                getPlayerData().save(getPlayerDataFile());
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
+                try {
+                    getPlayerData().save(getPlayerDataFile());
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
             }
         }
     }
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent e){
+    public void onJoin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
         String uuid = player.getUniqueId().toString();
 
-        if (!getPlayerData().contains(uuid)){
+        if (!getPlayerData().contains(uuid)) {
             getPlayerData().createSection(uuid);
             getPlayerData().createSection(uuid + ".playerName");
             getPlayerData().createSection(uuid + ".diamondOreMined");
--- src/main/java/com/orf9/antixrayplus/commandAntiXrayPlus.java
+++ src/main/java/com/orf9/antixrayplus/commandAntiXrayPlus.java
@@ -71,7 +71,58 @@ public class commandAntiXrayPlus implements CommandExecutor {
                 player.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
             }
         } else {
-            sender.sendMessage("This command can only be used in-game!");
+            CommandSender player = sender;
+
+                if (args.length == 1) {
+                    Player target = Bukkit.getPlayer(args[0]);
+                    if (target != null) {
+                        String uuid = target.getUniqueId().toString();
+
+                        int stoneMined = (int) main.getPlayerData().get(uuid + ".stoneMined");
+                        int diamondOreMined = (int) main.getPlayerData().get(uuid + ".diamondOreMined");
+
+                        if (!(diamondOreMined == 0) && !(stoneMined == 0)) {
+                            double ratio = (double) main.getPlayerData().get(uuid + ".ratio");
+
+                            player.sendMessage("Blocks mined by: " + target.getName());
+                            player.sendMessage("Diamond Ore: " + diamondOreMined);
+                            player.sendMessage("Stone: " + stoneMined);
+                            player.sendMessage("Ratio of Diamond to Stone: " + ratio);
+
+                            double maxRatio = main.getConfig().getDouble("maxRatio");
+
+                            int minStone = main.getConfig().getInt("stoneMinimum");
+                            int minDiamondOre = main.getConfig().getInt("DiamondMinimum");
+
+                            if (minStone == 0){
+                                minStone = 1024;
+                            }
+                            if (minDiamondOre == 0){
+                                minDiamondOre = 32;
+                            }
+                            if (maxRatio == 0){
+                                maxRatio = 0.002;
+                            }
+
+                            if (ratio > maxRatio && stoneMined > minStone && diamondOreMined > minDiamondOre) {
+                                player.sendMessage(ChatColor.RED + "Player may be using XRAY! Ratio: " + ratio);
+                            }
+                        }else {
+                            player.sendMessage("Blocks mined by: " + target.getName());
+                            player.sendMessage("Diamond Ore: " + diamondOreMined);
+                            player.sendMessage("Stone: " + stoneMined);
+                            player.sendMessage("Not enough data yet to display ratio!");
+                        }
+
+
+
+                    } else {
+                        player.sendMessage("You must specify a valid player!");
+                    }
+
+                } else {
+                    player.sendMessage("Invalid Arguments! Usage: /antixrayplus [player]");
+                }
         }
         return false;
     }
